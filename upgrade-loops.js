aa27a8bf86546760de5bd9301889328ac099d638
Celia Ballesteros <cebalgar@gmail.com>
2021-11-05 12:10:35 +0100

    Iteration#4

--- upgrade-loops.js
+++ upgrade-loops.js
@@ -17,3 +17,15 @@ for (var places of placesToTravel) {
 	console.log(places);
   }
 
+//-----------------------------------------------------------
+
+const alien = {
+  name: 'Wormuck',
+  race: 'Cucusumusu',
+  planet: 'Eden',
+  weight: '259kg'
+}
+  
+for (var key in alien) {
+  console.log("Alien tiene " + key + " con valor: " + alien[key]);
+}
