26a78fe8fa96d43b3a47b7eb4aec9ff80515dfd6
Celia Ballesteros <cebalgar@gmail.com>
2021-11-05 12:15:47 +0100

    Iteration#6

--- upgrade-loops.js
+++ upgrade-loops.js
@@ -50,3 +50,23 @@ for(let index=0; index<placesToTravel.length; index++){
 console.log(placesToTravel);
 
 //------------------------------------------------------------
+const toys = [
+  {id: 5, name: 'Buzz MyYear'}, 
+  {id: 11, name: 'Action Woman'}, 
+  {id: 23, name: 'Barbie Man'}, 
+  {id: 40, name: 'El gato con Guantes'},
+  {id: 40, name: 'El gato felix'}
+  ]
+  let nombreSinGato=[];
+  
+  for(let toy of toys){
+
+    if(!(toy.name).includes("gato"))
+
+    nombreSinGato.push(toy);
+   
+    console.log(nombreSinGato);
+  }
+ 
+
+//----------------------------------------------------
