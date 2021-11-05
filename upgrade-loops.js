c472c3a06d2496163428a42e31a8cb95d82e6606
Celia Ballesteros <cebalgar@gmail.com>
2021-11-05 12:14:21 +0100

    Iteration#5

--- upgrade-loops.js
+++ upgrade-loops.js
@@ -29,3 +29,24 @@ const alien = {
 for (var key in alien) {
   console.log("Alien tiene " + key + " con valor: " + alien[key]);
 }
+//-----------------------------------------------------------------
+const placesToTravel = 
+[{id: 5, name: 'Japan'}, 
+{id: 11, name: 'Venecia'}, 
+{id: 23, name: 'Murcia'}, 
+{id: 40, name: 'Santander'}, 
+{id: 44, name: 'Filipinas'}, 
+{id: 59, name: 'Madagascar'}]
+
+
+for(let index=0; index<placesToTravel.length; index++){
+  
+  if(placesToTravel[index].id === 11){
+    placesToTravel.splice(index, 1)
+  }else if(placesToTravel[index].id ===40)
+  placesToTravel.splice(index, 1)
+
+}
+console.log(placesToTravel);
+
+//------------------------------------------------------------
